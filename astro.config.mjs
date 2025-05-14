30680f7476b4237c00df1e8cacf345d79484e7bc
nikdelvin <the@nikdelv.in>
2025-05-15 02:36:20 +0400

    Dev Toolbar Off

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://scripty.by.nikdelv.in',
-    integrations: [tailwind()]
+    integrations: [tailwind()],
+    devToolbar: {
+        enabled: false
+    }
 })
